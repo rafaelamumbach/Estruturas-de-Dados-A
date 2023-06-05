b6d209d25ea7c0ad279dad60421628c87cb68121
Rafaela Mumbach Buenos <114167659+rafaelamumbach@users.noreply.github.com>
2023-06-05 14:58:50 -0300

    Update atividade3.c

--- ListasParte1/atividade3.c
+++ ListasParte1/atividade3.c
@@ -17,40 +17,36 @@ void lst_imprime (Lista* l);
 void lst_libera (Lista* l);
 Lista* ultimo (Lista* l);
 
-/* função de criação: retorna uma lista vazia */
-Lista* lst_cria (void)
-{
+//função de criação: retorna uma lista vazia
+Lista* lst_cria (void){
 	return NULL;
 }
 
-/* inserção no início: retorna a lista atualizada */
-Lista* lst_insere (Lista* l, int i)
-{
+//inserção no início: retorna a lista atualizada
+Lista* lst_insere (Lista* l, int i){
 	Lista* novo = (Lista*) malloc(sizeof(Lista));
 	novo->info = i;
 	novo->prox = l;
 	return novo;
 }
 
-/* função imprime: imprime valores dos elementos */
-void lst_imprime (Lista* l)
-{
+//função imprime: imprime valores dos elementos
+void lst_imprime (Lista* l){
 	Lista* p;
 	for (p = l; p != NULL; p = p->prox)
 		printf("info = %d\n", p->info);
 }
-void lst_libera (Lista* l)
-{
+
+void lst_libera (Lista* l){
 	Lista* p = l;
 	while (p != NULL) {
-		l = p->prox; /* guarda referência p/ próx. elemento */
-		free(p); /* libera a memória apontada por p */
-	p = l; /* faz p apontar para o próximo */
+		l = p->prox; //guarda referência p/ próx. elemento
+		free(p); //libera a memória apontada por p
+	p = l; //faz p apontar para o próximo
 	}
 }
 
-Lista* ultimo (Lista* l)
-{
+Lista* ultimo (Lista* l){
 	Lista* p=l;
 	if (p!=NULL)
 		while (p->prox!=NULL)
@@ -58,14 +54,13 @@ Lista* ultimo (Lista* l)
 	return p;
 }
 
-int main (void)
-{
-	Lista* l; /* declara uma lista não iniciada */
-	l = lst_cria(); /* inicia lista vazia */
-	l = lst_insere(l, 23); /* insere na lista o elemento 23 */
-	l = lst_insere(l, 45); /* insere na lista o elemento 45 */
-	l = lst_insere(l, 56); /* insere na lista o elemento 56 */
-	l = lst_insere(l, 78); /* insere na lista o elemento 78 */
+int main (void){
+	Lista* l; //declara uma lista não iniciada
+	l = lst_cria(); //inicia lista vazia
+	l = lst_insere(l, 23); //insere na lista o elemento 23
+	l = lst_insere(l, 45); //insere na lista o elemento 45
+	l = lst_insere(l, 56); //insere na lista o elemento 56
+	l = lst_insere(l, 78); //insere na lista o elemento 78
 	Lista* ult = ultimo (l);
 	if (ult!=NULL)
 		printf ("\nO ultimo no da lista eh o %d no endereco %p e o proximo eh null (%p)\n", ult->info, ult, ult->prox);
